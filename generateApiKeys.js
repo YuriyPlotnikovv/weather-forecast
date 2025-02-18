6667cdb1ffe171c048839c9b039e1edb7ae2d130
Yuriy Plotnikov <yuriy.plotnikov@travelline.ru>
2025-02-18 19:35:27 +0300

    Генерация ключей на сервере

--- generateApiKeys.js
+++ generateApiKeys.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const filePath = path.join(__dirname, 'core', 'apiKeys.php');
 const content = `<?php
-$ipKey = '{process.env.IP_KEY}';
-$timeKey = '{process.env.TIME_KEY}';
+$ipKey = '${process.env.IP_KEY}';
+$timeKey = '${process.env.TIME_KEY}';
 `;
 
 fs.writeFileSync(filePath, content, 'utf8');
