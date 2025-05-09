be0030123bfc495f98cf7b611442233e0845005a
Yuriy Plotnikov <yuriy.plotnikov@travelline.ru>
2025-05-09 10:18:29 +0300

    Настройка GitHub Actions

---
+++ .github/scripts/generateReadme.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const path = require('path');
+
+const projectJsonPath = path.join(__dirname, '..', '..', '.info', 'project.json');
+const templatePath = path.join(__dirname, '..', '..', '.info', 'README-template.md');
+const readmePath = path.join(__dirname, '..', '..', 'README.md');
+
+function formatDesc(desc) {
+  if (Array.isArray(desc)) {
+    return desc.map(d => `- ${d}`).join('\n');
+  }
+  return desc;
+}
+
+function featuresList(data, lang) {
+  if (!Array.isArray(data.features)) return '';
+  return data.features.map(f => {
+    const name = f[`name-${lang}`];
+    const desc = f[`description-${lang}`];
+    if (!name || !desc) return '';
+
+    if (Array.isArray(desc)) {
+      const descList = desc.map(item => `- ${item}`).join('\n');
+      return `#### ${name}:\n\n${descList}`;
+    }
+
+    return `#### ${name}:\n\n- ${desc}`;
+  }).filter(Boolean).join('\n\n');
+}
+
+function fillTemplate(template, vars) {
+  return template.replace(/{{\s*([\w-]+)\s*}}/g, (_, key) => vars[key] || '');
+}
+
+function generateReadme() {
+  if (!fs.existsSync(projectJsonPath)) {
+    console.error('project.json не найден');
+    process.exit(1);
+  }
+  if (!fs.existsSync(templatePath)) {
+    console.error('Шаблон README-template.md не найден');
+    process.exit(1);
+  }
+
+  const rawData = fs.readFileSync(projectJsonPath, 'utf8');
+  const data = JSON.parse(rawData);
+
+  const template = fs.readFileSync(templatePath, 'utf8');
+
+  const posterImg = `<img src=".info/poster.webp" alt="Poster" width="600" />`;
+
+  const vars = {
+    'title-en': data['title-en'],
+    'title-ru': data['title-ru'],
+    'textFirst-en': data['textFirst-en'],
+    'textFirst-ru': data['textFirst-ru'],
+    'textSecond-en': data['textSecond-en'],
+    'textSecond-ru': data['textSecond-ru'],
+    'features-en': featuresList(data, 'en'),
+    'features-ru': featuresList(data, 'ru'),
+    'deploy': data.deploy,
+    'poster-img': posterImg
+  };
+
+  const readme = fillTemplate(template, vars);
+
+  fs.writeFileSync(readmePath, readme, 'utf8');
+  console.log('README.md сгенерирован из шаблона');
+}
+
+generateReadme();
---
+++ .info/README-template.md
@@ -0,0 +1,67 @@
+# {{title-en}}
+
+{{poster-img}}
+
+[🇬🇧 English](#english) | [🇷🇺 Русский](#русский)
+
+---
+
+## English
+
+### {{title-en}}
+
+Project: {{deploy}}
+
+{{textFirst-en}}
+
+{{textSecond-en}}
+
+---
+
+### About the project
+
+{{features-en}}
+
+---
+
+### License
+
+This project is licensed under the [GNU Affero General Public License v3 (AGPLv3)](https://www.gnu.org/licenses/agpl-3.0.html).
+
+---
+
+### Contacts
+
+Author: Yuriy Plotnikov  
+Website: https://yuriyplotnikovv.ru  
+
+---
+
+## Русский
+
+### {{title-ru}}
+
+Проект: {{deploy}}
+
+{{textFirst-ru}}
+
+{{textSecond-ru}}
+
+---
+
+### О проекте
+
+{{features-ru}}
+
+---
+
+### Лицензия
+
+Проект распространяется под лицензией [GNU Affero General Public License v3 (AGPLv3)](https://www.gnu.org/licenses/agpl-3.0.html).
+
+---
+
+### Контакты
+
+Автор: Yuriy Plotnikov  
+Сайт: https://yuriyplotnikovv.ru  
